bb4c64a31db17d07cb313f8fafc4986691b184e3
Claire Oliver <doliver@rocketsoftware.com>
2026-03-10 09:49:22 -0500

    Added front end skeleton

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import HomePage from './HomePage.vue'
+</script>
+
+<template>
+  <HomePage msg="Welcome to NXT TCG" />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
