ffa1dc6c0fb8343b10cf6bfb47631b20129636cd
rocketchristy <cdunlap@rocketsoftware.com>
2026-03-11 16:10:00 -0500

    Added User endpoints

--- Backend/RestAPI/Routes/inventory.py
+++ Backend/RestAPI/Routes/inventory.py
@@ -2,4 +2,6 @@ from fastapi import APIRouter, Body
 from Backend.Utilities.logger import logger
 
 router = APIRouter()
-
+'''
+@router.get("/")
+'''
--- Backend/Utilities/utilities.py
+++ Backend/Utilities/utilities.py
@@ -1,5 +1,13 @@
 import hashlib
+from Backend.Utilities.logger import logger
+from fastapi import Header, HTTPException
 
 def hash_password(password: str) -> str:
     """Hash a password using SHA-256."""
     return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+def get_token_header(token: str = Header(...)):
+    if not token:
+        raise HTTPException(status_code=401, detail="Token header missing")
+    # Optionally, add logic to validate the token here
+    return token
